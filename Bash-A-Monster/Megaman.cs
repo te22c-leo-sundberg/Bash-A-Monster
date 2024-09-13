a7e1d83377fc03b43d84ba242df644c3c52c6863
Leo Sundberg <leo.sundberg@elev.ga.ntig.se>
2024-09-13 12:32:06 +0200

    first commit

---
+++ Bash-A-Monster/Megaman.cs
@@ -0,0 +1,5 @@
+public class Megaman 
+{
+    public string Name;
+    public int Hitpoints;    
+}
---
+++ Bash-A-Monster/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design.Serialization;
+using System.Threading.Tasks.Dataflow;
+using Microsoft.VisualBasic;
+
+Random rnd = new Random();
+Megaman man = new Megaman()
+{
+    Name = "Rockman",
+    Hitpoints = 100
+};
+
+Megaman zero = new Megaman()
+{
+    Name = "Zero",
+    Hitpoints = 100
+};
+
+while (zero.Hitpoints > 0)
+{
+    Console.WriteLine($"{man.Name} has {man.Hitpoints} health!");
+    Console.WriteLine($"{zero.Name} has {zero.Hitpoints} health!");
+    Console.WriteLine("Attack by pressing [Q]!");
+    string input = Console.ReadLine().ToUpper();
+    if (input == "Q")
+    {
+    int damage = rnd.Next(6, 17);
+    zero.Hitpoints -= damage;
+    Console.WriteLine($"{zero.Name} has {zero.Hitpoints} health!");
+    }
+}
+Console.ReadLine();
